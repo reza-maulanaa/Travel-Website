419ee2649c679d140f3fe008f2046f90fa274ecf
Reza <rezzreborn@gmail.com>
2026-07-19 21:11:20 +0700

    tahap 1

---
+++ src/lib/types/index.ts
@@ -0,0 +1,11 @@
+export interface Trip {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  duration: string;
+  image_url: string;
+  max_slots: number;
+  available_slots: number;
+  whatsapp_number: string;
+}
